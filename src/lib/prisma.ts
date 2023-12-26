bd33b84b0cdd5cca8cf7704700d52764efa685dd
Tushar Puri <92613357+TusharPuri10@users.noreply.github.com>
2023-12-26 22:44:03 +0530

    edited prisma lib

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../prisma/generated/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-const prisma = global.prisma || new PrismaClient();
+const prisma = new PrismaClient();
 
 export default prisma;
