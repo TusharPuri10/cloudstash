4eff50f4c2bf2b33edd8a71f3c08b1c831e476bc
Tushar Puri <92613357+TusharPuri10@users.noreply.github.com>
2023-12-27 11:52:37 +0530

    added redirect url option

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,15 +1,23 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
+// import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "@/lib/prisma";
-
 
 export const authOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma as any),
   theme: {
     logo: "/icons/logo.jpg",
+  },
+  callbacks: {
+    async redirect({ url, baseUrl }: { url: string, baseUrl: string }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url
+      return baseUrl
+    }
   },
   providers: [
     // GoogleProvider({
