98be96e0173949e2820c29a7fb4dcfd6297b91d2
Tushar Puri <92613357+TusharPuri10@users.noreply.github.com>
2023-12-27 13:42:09 +0530

    minor change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function App() {
         <h1 className="flex justify-center items-center px-4 pt-4 mt-8 mr-4 mb-2 text-2xl font-extrabold text-gray-900 dark:text-white md:text-4xl lg:text-5xl xl:text-6xl">
           A cloud-based storage service
         </h1>
-        <p className="flex justify-center items-center text-base font-normal text-white md:text-lg lg:text-xl">
+        <p className="flex justify-center items-center text-base font-normal md:text-lg text-white  lg:text-xl">
           Enter the box
         </p>
       </div>
