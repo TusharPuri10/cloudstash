1d8ba41516e30b964ca0c2f741b1781c2fb93d2e
Tushar Puri <92613357+TusharPuri10@users.noreply.github.com>
2023-12-29 22:10:39 +0530

    added favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
@@ -9,6 +10,13 @@ export default function App({ Component, pageProps }: AppProps) {
     <SessionProvider session={pageProps.session}>
       <RecoilRoot>
         <AnimatePresence>
+        <Head>
+        <title>Cloud Stash</title>
+          <link rel="shortcut icon" href="/favicon/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+        </Head>
           <Component {...pageProps} />
         </AnimatePresence>
       </RecoilRoot>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import CloudAndBox from "@/components/3dmodels/CloudAndBox";
 import NextNProgress from "nextjs-progressbar";
 import Topbar from "@/components/UI/Topbar";
@@ -7,10 +6,6 @@ export default function App() {
   return (
     <div className="relative z-0 w-full h-screen">
       <NextNProgress color="#FFB000" />
-      <Head>
-        <title>Cloud Stash</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <div className="absolute inset-0 z-50 h-20">
         <Topbar />
         <h1 className="flex justify-center items-center px-4 pt-4 mt-8 mr-4 mb-2 text-2xl font-extrabold text-gray-900 dark:text-white md:text-4xl lg:text-5xl xl:text-6xl">
